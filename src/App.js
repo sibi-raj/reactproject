f597c9579822408011b4280ca85b9915e5256522
sibi-raj <sibiraj1817@gmail.com>
2023-11-17 14:45:16 +0530

    inlinestyles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from "react";
-
-
-import Message from "./day 3/Message";
-
-function App() {
+function Apps() {
   return (
-    <div>
-      <Message></Message>
+    <div style={{ backgroundColor: "lightblue", padding: "10px" }} id="Border">
+      <h1 style={{ color: "green" }}>Inline Style in JSX Example.</h1>
+      <p style={{ color: "blue", marginTop: "3px", fontSize: "16px" }}>
+        This is a paragraph with inline styles applied.
+      </p>
     </div>
-  )
+  );
 }
 
-export default App;
+export default Apps;
